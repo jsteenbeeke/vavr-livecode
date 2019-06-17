5a88e4b4d7bf3cdc9b385d2004eb335ff93fe032
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2019-06-17 17:53:22 +0200

    Updated answers with changes in presentation

--- src/test/java/nl/topicus/topiconf/vavr/livecode/Demo2.java
+++ src/test/java/nl/topicus/topiconf/vavr/livecode/Demo2.java
@@ -23,29 +23,25 @@ public class Demo2 extends DemoMetAdressen
 		// Voeg 3 vrijwilligers toe
 		vrijwilliger1 = new Persoon("Jeroen", "Steenbeeke");
 		vrijwilliger2 = new Persoon("Martijn", "Dashorst");
-		vrijwilliger3 = new Persoon("Emond", "Papegaaij");
+		vrijwilliger3 = new Persoon("Thijs", "Kupers");
 	}
 
 
 	@Test
-	public void filterAndReject() {
-		// 1. zet alle personen in een Array
+	public void rejections() {
+		// 1. Zet alle personen in een Array
 		Array<Persoon> personen = Array.of(vrijwilliger1, vrijwilliger2, vrijwilliger3);
 
-		// 2. gebruik filter om 1 persoon te selecteren
-		var filtered = personen.filter(p -> p.getVoornaam().equals("Jeroen"));
+		// 2. Neem de voornamen van iedereen
+		var voornamen = personen.map(Persoon::getVoornaam);
 
-		// 3. gebruik rejected om 1 persoon te verwijderen
-		var rejected = personen.reject(p -> p.getVoornaam().equals("Jeroen"));
+		// 3. Reject alle Jeroens
+		var nietJeroen = voornamen.reject("Jeroen"::equals);
 
-		assertTrue(filtered.contains(vrijwilliger1));
-		assertFalse(filtered.contains(vrijwilliger2));
-		assertFalse(filtered.contains(vrijwilliger3));
-
-		assertFalse(rejected.contains(vrijwilliger1));
-		assertTrue(rejected.contains(vrijwilliger2));
-		assertTrue(rejected.contains(vrijwilliger3));
-
+		// 4. Controleer dat er nog 2 vrijwilligers over zijn
+		assertThat(nietJeroen.size(), equalTo(2));
+		assertTrue(nietJeroen.contains("Thijs"));
+		assertTrue(nietJeroen.contains("Martijn"));
 	}
 
 	@Test
@@ -62,27 +58,5 @@ public class Demo2 extends DemoMetAdressen
 		// 4. bewijs dat elk persoon nu 1 adres heeft
 		assertThat(personenMetAdressen.size(), equalTo(3));
 		personenMetAdressen.forEach(p -> assertThat(p.getAdressen().size(), equalTo(1)));
-	}
-
-	@Test
-	public void union_intersect_diff() {
-		// 1. maak een Set van vrijwilligers 1 en 2
-		HashSet<Persoon> eenEnTwee = HashSet.of(vrijwilliger1, vrijwilliger2);
-
-		// 2. maak een Set van vrijwilligers 2 en 3
-		HashSet<Persoon> tweeEnDrie = HashSet.of(vrijwilliger2, vrijwilliger3);
-
-		// 3. Maak een set van alle vrijwilligers
-		HashSet<Persoon> allemaal = HashSet.of(vrijwilliger1, vrijwilliger2, vrijwilliger3);
-
-		// 4. Demonstreer dat union vrijwilligers 1, 2 en 3 oplevert
-		assertThat(eenEnTwee.union(tweeEnDrie), equalTo(allemaal));
-
-		// 5. Demonstreer dat intersect vrijwilliger 2 oplevert
-		assertThat(eenEnTwee.intersect(tweeEnDrie), equalTo(HashSet.of(vrijwilliger2)));
-
-		// 6. Demonstreer dat diff vrijwilliger 1 respectievelijk 3 oplevert
-		assertThat(eenEnTwee.diff(tweeEnDrie), equalTo(HashSet.of(vrijwilliger1)));
-		assertThat(tweeEnDrie.diff(eenEnTwee), equalTo(HashSet.of(vrijwilliger3)));
 	}
 }
